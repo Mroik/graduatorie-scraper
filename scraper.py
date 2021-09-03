0e898c54c41d30a1b1d12b278c0591910d7b504d
mroik <mroik@delayed.space>
2021-09-03 16:56:25 +0200

    Working usable version
    For now the id increment is hardcoded as 7, this might be always +7 or
    it could morelikely be +len(ids).
    To know for sure more testing with a different list of "graduatorie" is
    needed.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,3 @@
-import datetime
-
 import requests
 import re
 from datetime import datetime as dt
@@ -8,8 +6,6 @@ from bs4 import BeautifulSoup as bs
 
 BASE_URL = "https://studente.unimi.it/ammissioni/g/graduatoriaprogrammati/"
 ENTRY = "checkLogin.asp"
-
-sess = requests.Session()
 
 
 def find_updater_links(data):
@@ -37,23 +33,22 @@ def find_pdf(data):
     return ris
 
 
-def main():
-    # TODO Remove logging [DEBUG]
+def scrape(count):
+    sess = requests.Session()
+
     data = sess.get(BASE_URL + ENTRY).text
     links, buttons = find_updater_links(data)  # buttons[0] => next, buttons[1] => previous
     ids = get_ids(data)
 
-    stamp = datetime.datetime.now().timestamp() * 1000
+    stamp = dt.now().timestamp() * 1000
     resp = sess.get(
-        BASE_URL + links[0] + "&_=" + str(stamp),
+        BASE_URL + links[count] + "&_=" + str(stamp),
         headers={
             "Wicket-Ajax": "true",
             "Wicket-Ajax-BaseURL": "g/graduatoriaprogrammati/checkLogin.asp?0",
             "X-Requested-With": "XMLHttpRequest"
         }
     )
-    with open("part1.txt", "w") as fd:
-        fd.write(resp.text)
 
     old_ids = ids
     ids = get_ids(resp.text)  # Every time an update is called the IDs change
@@ -63,21 +58,29 @@ def main():
         headers={
             "Wicket-Ajax": "true",
             "Wicket-Ajax-BaseURL": "g/graduatoriaprogrammati/checkLogin.asp?0",
-            "Wicket-FocusedElementId": f"{int(old_ids[0]) + 8}"
+            "Wicket-FocusedElementId": "{:x}".format(int(old_ids[count], 16) + 8)
         },
         data={
-            f"id{int(old_ids[0]) + 7}_hf_0": "",
+            "id{:x}_hf_0".format(int(old_ids[count]) + 7): "",  # This might be always +7 or it might be +len(ids)
             "buttons:next": "1"
         }
     )
-    with open("part2.txt", "w") as fd:
-        fd.write(resp.text)
 
     pdfs = find_pdf(resp.text)
     for x in pdfs:
         resp = sess.get(BASE_URL + x[1])
         with open(x[0], "wb") as fd:
             fd.write(resp.content)
+
+
+def main():
+    count = 0
+    try:
+        while True:
+            scrape(count)
+            count += 1
+    except IndexError:
+        return
 
 
 if __name__ == "__main__":
